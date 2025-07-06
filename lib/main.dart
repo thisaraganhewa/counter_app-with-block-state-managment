200217df24c10ffc415a479a607f325140d1ed2d
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2025-07-06 22:12:00 +0530

    added multibloc provider to send multiple blocs

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:counter_app/bloc/counter_bloc.dart';
 import 'package:counter_app/cubit/counter_cubit.dart';
 import 'package:counter_app/home_page.dart';
 import 'package:flutter/material.dart';
@@ -12,8 +13,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => CounterCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => CounterCubit(),),
+        BlocProvider(create: (_) => CounterBloc())
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
