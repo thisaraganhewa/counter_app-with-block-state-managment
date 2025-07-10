a6fcae4719631ac3839655f95e50561caebb540c
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2025-07-10 21:38:29 +0530

    removed create from block build cuz it is not nesdsasry also the variables

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -20,9 +20,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
 
-    //final counter = counterCubit.state;
-    final counterCubit = BlocProvider.of<CounterCubit>(context);
-    final counterBloc = BlocProvider.of<CounterBloc>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
